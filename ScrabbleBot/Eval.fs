824c0be0042068503a97e2bb9c7fb964684ada14
ItzWeihao <weihaoandersen@gmail.com>
2023-04-12 11:24:11 +0200

    Eval modification

--- ScrabbleBot/Eval.fs
+++ ScrabbleBot/Eval.fs
@@ -86,11 +86,91 @@ module internal Eval
         
     let prog = new StateBuilder()
 
-    let arithEval2 a = failwith "Not implemented"
-    let charEval2 c = failwith "Not implemented"
-    let rec boolEval2 b = failwith "Not implemented"
+           
+    let rec arithEval2 a =
+        match a with
+        | N n -> prog { return n }
+        | V v -> prog { return! lookup v }
+        | WL -> prog { return! wordLength }
+        | PV pv -> prog { let! a = arithEval pv
+                          return! pointValue a }
+        | Add(a, b) -> prog { let! x = (arithEval2 a)
+                              let! y = (arithEval2 b)
+                              return (( + ) x y)}
+        | Sub(a, b) -> prog { let! x = (arithEval2 a)
+                              let! y = (arithEval2 b)
+                              return (( - ) x y)}
+        | Mul(a, b) -> prog { let! x = (arithEval2 a)
+                              let! y = (arithEval2 b)
+                              return (( * ) x y)}
+        | Div(a, b) -> prog { let! x = (arithEval2 a)
+                              let! y = (arithEval2 b)
+                              if y <> 0
+                              then return (( / ) x y)
+                              else return! fail DivisionByZero}
+        | Mod(a, b) -> prog { let! x = (arithEval2 a)
+                              let! y = (arithEval2 b)
+                              if y <> 0
+                              then return (( % ) x y)
+                              else return! fail DivisionByZero}
+        | CharToInt c -> prog { let! i = charEval2 c
+                                return int i }
+    
+    and charEval2 c =
+        match c with
+        | C c -> prog { return c }
+        | CV a -> prog { let! a1 = arithEval2 a
+                         return! characterValue a1 }
+        | ToUpper c -> prog { let! c1 = charEval2 c
+                              return System.Char.ToUpper c1 }
+        | ToLower c -> prog { let! c1 = charEval2 c
+                              return System.Char.ToLower c1 }
+        | IntToChar a -> prog { let! a1 = arithEval2 a
+                                return char (a1 + int '0') }
+    let rec boolEval2 b =
+        match b with
+        | TT -> prog { return true }
+        | FF -> prog { return false }
+        
+        | AEq(a, b) -> prog { let! a1 = arithEval2 a
+                              let! b1 = arithEval2 b
+                              return (( = ) a1 b1) }
+        | ALt(a, b) -> prog { let! a1 = arithEval2 a
+                              let! b1 = arithEval2 b
+                              return (( < ) a1 b1) }
+        | Not(b) -> prog { let! b1 = boolEval2 b
+                           return not b1 }
+        | Conj(b1, b2) -> prog { let! b3 = boolEval2 b1
+                                 let! b4 = boolEval2 b2
+                                 return (( && ) b3 b4) }
+        
+        | IsVowel(c) -> prog { let! c1 = charEval2 c
+                               return "aeiouæøåAEIOUÆØÅ".Contains(c1) }
+        | IsConsonant(c) -> prog { let! c1 = charEval2 c
+                                   return not ("aeiouæøåAEIOUÆØÅ".Contains(c1)) }
 
-    let stmntEval2 stm = failwith "Not implemented"
+    let rec stmntEval2 stm =
+        match stm with
+        | Declare s -> prog { return! declare s }
+        | Ass(s, a) -> prog { let! a1 = arithEval2 a
+                              return! update s a1 }
+        | Skip -> prog { return () }
+        | Seq(s1, s2) -> prog { do! stmntEval2 s1
+                                let! s3 = stmntEval2 s2
+                                return s3}
+        | ITE(b, s1, s2) -> prog { let! bool = boolEval2 b
+                                   do! push
+                                   if bool
+                                   then do! stmntEval2 s1
+                                   else do! stmntEval2 s2
+                                   do! pop}
+        | While(b, s) -> prog { let! bool = boolEval2 b
+                                do! push
+                                if bool
+                                then do! stmntEval2 s
+                                     do! stmntEval2 (While (b, s))
+                                else return ()
+                                do! pop }
 
 (* Part 4 *) 
 
